6e3a781ee427a905961330caa70f54a7b91ed50f
Mann Mehta <17it051@charusat.edu.in>
2020-04-20 14:55:01 +0530

    Added solution to Monty Hall Paradox 2.0

--- Monty Hall Paradox 2.0/Solution/Monty Hall Paradox.cpp
+++ Monty Hall Paradox 2.0/Solution/Monty Hall Paradox.cpp
@@ -1,12 +1,19 @@
+/// ILLUMINATI SEASON 6
+/// Monty Hall Paradox 2.0
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+
+#define FAST_IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
 int main(){
+    FAST_IO
     ll t;
     cin>>t;
     while(t--){
         ll n;
         cin>>n;
-
+        cout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
     }
 }
