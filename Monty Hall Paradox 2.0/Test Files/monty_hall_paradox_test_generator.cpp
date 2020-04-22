eb6ecd2ba4041e6787168a13653d24423661420d
Mann Mehta <17it051@charusat.edu.in>
2020-04-22 09:18:40 +0530

    Updated test files of the Monty Hall Paradox 2.0

--- Monty Hall Paradox 2.0/Test Files/monty_hall_paradox_test_generator.cpp
+++ Monty Hall Paradox 2.0/Test Files/monty_hall_paradox_test_generator.cpp
@@ -1,5 +1,5 @@
 /// ILLUMINATI SEASON 6
-/// Monty Hall Paradox 2.0
+/// Monty Hall Paradox 2.0 (Test Files Generator)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -17,13 +17,74 @@ int main(){
     fin.open("Input_File_1.txt");
     fout.open("Output_File_1.txt");
     ll t;
-    t = (ll)1e5;
-    fin<<t<<endl;
+    t = 10000;
     while(t--){
-        ll n = randRange(3,((ll)(1e18)));
+        ll n = randRange(3,100);
         fin<<n<<endl;
         fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
     }
+    t = 10000;
+    while(t--){
+        ll n = randRange(100,1000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(1000,10000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(10000,100000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(100000,1000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(1000000,10000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(10000000,100000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange(100000000,1000000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000;
+    while(t--){
+        ll n = randRange((1000000000-10000),1000000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+    t = 10000-2;
+    while(t--){
+        ll n = randRange(3,1000000000);
+        fin<<n<<endl;
+        fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    }
+
+    /// Boundary Test Cases
+    ll n = 3;
+    fin<<n<<endl;
+    fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
+    n = 1000000000;
+    fin<<n<<endl;
+    fout<<1<<" "<<n<<" "<<n-1<<" "<<n<<endl;
     fout.close();
     fin.close();
 }
